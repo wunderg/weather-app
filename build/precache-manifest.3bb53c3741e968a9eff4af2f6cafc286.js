3df49a8ca7ca764597f0a6ef3bc255493e33c79a
oumarov <oumarov@visa.com>
2020-04-12 20:51:17 -0500

    add latest fiels

---
+++ build/precache-manifest.3bb53c3741e968a9eff4af2f6cafc286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86af7134aa4055259d608f66833443a",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6c07999c1e749bb8a631",
+    "url": "/weather-app/static/css/main.7b45cd59.chunk.css"
+  },
+  {
+    "revision": "189bad0ed36738db1976",
+    "url": "/weather-app/static/js/2.02f2389f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.02f2389f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c07999c1e749bb8a631",
+    "url": "/weather-app/static/js/main.b4db0988.chunk.js"
+  },
+  {
+    "revision": "80a71bacf6ca8d094b30",
+    "url": "/weather-app/static/js/runtime-main.01d02953.js"
+  }
+]);
