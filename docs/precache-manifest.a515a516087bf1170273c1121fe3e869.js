f27c1b4453989c6a36b07251a81621a79143fced
oumarov <oumarov@visa.com>
2020-04-12 20:43:08 -0500

    add files

---
+++ docs/precache-manifest.a515a516087bf1170273c1121fe3e869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8a864199d6ae2ff3940b012fdf9fac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbe4ce681daea09124f5",
+    "url": "/static/css/main.7b45cd59.chunk.css"
+  },
+  {
+    "revision": "189bad0ed36738db1976",
+    "url": "/static/js/2.02f2389f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02f2389f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe4ce681daea09124f5",
+    "url": "/static/js/main.38d7a18e.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  const [coords, setCoords] = useState(false);
+  const [results, setResults] = useState({});
+//  const numberOfDays = useState(7);
+  const days = ["Sunday", 'Monday', 'Tuesday', "Wednesday", "Thrusday", "Friday", "Saturday"]
+
+  useEffect(() => {
+    if (!coords) {
+      navigator.geolocation.getCurrentPosition(
+        c => {
+          setCoords({latitude: c.coords.latitude, longitude: c.coords.longitude});
+        }, 
+        error => {
+          console.log(error);
+          setCoords(null)
+        }, 
+        {enableHighAccuracy:true,maximumAge:600000, timeout:10000}
+      );
+
+    } else {
+      getWeather({...coords, days: 7})
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); 
+        return data
+      })
+      .then(data => setResults(data))
+      .catch(e => console.log(e))
+    }
+  }, [coords]);
+
+  return (
+    <div className="App">
+    <div className="App-wrapper">
+    {Object.keys(results).length ?
+      <>
+    <div className="App-city">{results.location.name}</div>
+    <div className="App-condition">{results.current.condition.text}</div>
+    <div className="App-temp">{results.current.temp_f}</div>
+
+    <div className="App-line" />
+
+    {results.forecast.forecastday.map((i, index) => (
+      <div key={index} className="App-item">
+      <div className="App-day">{days[new Date(i.date).getDay()]}</div>
+      <img alt="weather-icon" src={i.day.condition.icon} />
+      <div className="App-temp2">{i.day.avgtemp_f}</div>
+      </div>
+
+    ))}
+
+    </>
+    : coords === null ? <div>Please Enable Location</div> : <div>Loading</div>
+    }
+    </div>
+    </div>
+  );
+}
+
+
+async function getWeather(props) {
+  return await fetch(`http://api.weatherapi.com/v1/forecast.json?key=fb0e6949c1e34887bac233528201204&q=${props.latitude.toFixed(2)},${props.longitude.toFixed(2)}&days=${props.days}`)
+}
+
+export default App;
